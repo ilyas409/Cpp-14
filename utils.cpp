5bdb780375901945c17a850ee52d8e1d00c4d4c2
Ilyas <ilyas1401408@gmail.com>
2019-02-28 11:23:13 -0800

    Fixes + suppor for fixed-size 2D arrays

--- utils.cpp
+++ utils.cpp
@@ -3,8 +3,10 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <array>
 #include <utility>  // std::pair, std::make_pair
 namespace own{
+    
     /**
      @brief: prints a single element
     */
@@ -21,7 +23,7 @@ namespace own{
         own::print(pair.first);
         own::print(": ");
         own::print(pair.second);
-        own::print("; ");
+        own::print(";");
     }
     /**
      @brief: prints a vector element
@@ -39,6 +41,8 @@ namespace own{
     */
     template <class T>
     void print(const std::vector<std::vector<T>> &m){
+        if (m.size() == 0) return;
+        print(m.size()); print(" x "); print(m[0].size());
         for (auto &v : m){
             own::print(v);
         }
@@ -64,33 +68,57 @@ namespace own{
             own::print(e);
             own::print(", ");
         }
-        own::print("}");
+        own::print("}\n");
     }
-};
-
-class Generic{
-public:
-   
-};
-
-int main() {
-    std::vector<std::vector<int>> matrix(10, {1,2,3});
-    own::print("matrix:\n");
-    own::print(matrix);
-    fix
-    std::vector<int> v(3, 1);
-    own::print("vector:\n");
-    own::print(v);
     
+    void print(const int *arr[]){
+        own::print(*arr[0]);
+        own::print("\n");
+    }
+    
+    template <size_t rows, size_t cols>
+    void print(int (&array)[rows][cols]){
+        print(rows); print(" x "); print(cols);
+        print("\n");
+        for (size_t i = 0; i < rows; ++i){
+            std::cout << i << ": ";
+            for (size_t j = 0; j < cols; ++j)
+                std::cout << array[i][j] << '\t';
+            std::cout << std::endl;
+        }
+    }
+    
+};
+int main() {
+   
+    std::vector<int> v(3, 1);
+    own::print("\nvector:\n");
     std::fill(v.begin(), v.end(), 0);
     own::print(v);
     
+    std::vector<std::vector<int>> matrixVector(3, {1,2,3});
+    own::print("\n2d vector:\n");
+    own::print(matrixVector);
+
     std::unordered_map<int, std::string> um = {{10, "a"},{20, "b"}};
-    own::print("unordered map:\n");
+    own::print("\nunordered map:\n");
     own::print(um);
-    
+
     std::unordered_set<int> us = {10, 20, 30};
-    own::print("unordered set:\n");
+    own::print("\nunordered set:\n");
     own::print(us);
+
+    own::print("\n2d array:\n");
+    int matrixArray[3][3];
+    memset(matrixArray, 0, sizeof(matrixArray[0][0]) * 3 * 3);
+    own::print(matrixArray);
+    
+    int matrixArray2[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+    
+    own::print(matrixArray2);
     
 }
