bc6979b3932f357564ea707a34f1ec270ee769a2
Ilyas <ilyas1401408@gmail.com>
2019-03-01 21:37:21 -0800

    Explicitly passing arguments to the base class

--- Polymorphic_task_queue_with_shared_pointers.cpp
+++ Polymorphic_task_queue_with_shared_pointers.cpp
@@ -3,29 +3,33 @@
 #include <memory>
 
 class BaseTask {
- public:
+  int complexity;
+public:
+  BaseTask(int complexity) : complexity(complexity){}
   virtual ~BaseTask() = default;
   virtual void executeTask() = 0;
 };
 
 class TaskA : public BaseTask {
- public:
+public:
+  TaskA(int complexity) : BaseTask(complexity){}
   void executeTask() { std::cout << "start TaskA\n"; }
 };
 
 class TaskB : public BaseTask {
- public:
+public:
+  TaskB(int complexity) : BaseTask(complexity){}
   void executeTask() { std::cout << "start TaskB\n"; }
 };
 
 class ObjectFactory {
- public:
+public:
   std::shared_ptr<BaseTask> createObject(int type) {
     switch (type) {
       case 0:
-        return std::make_shared<TaskA>();
+        return std::make_shared<TaskA>(0);
       case 1:
-        return std::make_shared<TaskB>();
+        return std::make_shared<TaskB>(1);
       default:
         return nullptr;
     }
@@ -34,14 +38,14 @@ class ObjectFactory {
 
 int main() {
   std::queue<std::shared_ptr<BaseTask>> taskQueue;
-
+  
   ObjectFactory factory;
   std::shared_ptr<BaseTask> taskA = factory.createObject(0);
   std::shared_ptr<BaseTask> taskB = factory.createObject(1);
-
+  
   taskQueue.push(taskA);
   taskQueue.push(taskB);
-
+  
   while (!taskQueue.empty()) {
     auto task = taskQueue.front();
     taskQueue.pop();
